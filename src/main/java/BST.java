1551f03e3e2a2e8e986cb4fb0efc40fc2aa40762
Artemenko Valerii <vatvalusha@gmail.com>
2017-10-18 17:03:13 +0300

    add methodes for Exercise_25 25.15

--- src/main/java/BST.java
+++ src/main/java/BST.java
@@ -1,10 +1,11 @@
+import java.util.ArrayList;
 import java.util.Iterator;
 
 /**
  * Created by Valera on 18.10.2017.
  */
 
-public class BST<E> extends AbstractTree<E> implements Cloneable {
+public class BST<E extends Comparable<E>> extends AbstractTree<E> implements Cloneable {
     @Override
     public boolean search(E e) {
         return false;
@@ -33,5 +34,31 @@ public class BST<E> extends AbstractTree<E> implements Cloneable {
     @Override
     public Iterator<E> iterator() {
         return null;
+    }
+
+    /** Returns the node for the specified element.
+     * Returns null if the element is not in the tree. */
+    private TreeNode<E> getNode(E element){
+        return null;
+    }
+
+    /** Returns true if the node for the element is a leaf */
+    private boolean isLeaf(E element){
+        return false;
+    }
+
+    public ArrayList<E> getPath(E e){
+        return null;
+    }
+
+    public static class TreeNode<E extends Comparable<E>> {
+        protected E element;
+        protected TreeNode<E> left;
+        protected TreeNode<E> right;
+        protected TreeNode<E> parent;
+
+        public TreeNode(E e) {
+            element = e;
+        }
     }
 }
