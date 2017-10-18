3e7eb85b13a173271475af52ed0a233c4ce94e64
Artemenko Valerii <vatvalusha@gmail.com>
2017-10-18 16:42:22 +0300

    Initial commit

---
+++ src/main/java/BST.java
@@ -0,0 +1,37 @@
+import java.util.Iterator;
+
+/**
+ * Created by Valera on 18.10.2017.
+ */
+
+public class BST<E> extends AbstractTree<E> implements Cloneable {
+    @Override
+    public boolean search(E e) {
+        return false;
+    }
+
+    @Override
+    public boolean insert(E e) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(E e) {
+        return false;
+    }
+
+    @Override
+    public void breadthFirstTraversal() {
+
+    }
+
+    @Override
+    public int getSize() {
+        return 0;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+}
